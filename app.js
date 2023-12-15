adaa0e7c8e99b43341c4cf79766de3ae24ad1f32
Harsh pal <113094328+Harsh17022004@users.noreply.github.com>
2023-12-15 14:38:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,3 @@
-function loco() {
-  gsap.registerPlugin(ScrollTrigger);
-  const locoScroll = new LocomotiveScroll({
-    el: document.querySelector("#main"),
-    smooth: true,
-    // for tablet smooth
-    tablet: { smooth: true },
-    // for mobile
-    smartphone: { smooth: true },
-  });
-  locoScroll.on("scroll", ScrollTrigger.update);
-
-  ScrollTrigger.scrollerProxy("#main", {
-    scrollTop(value) {
-      return arguments.length
-        ? locoScroll.scrollTo(value, 0, 0)
-        : locoScroll.scroll.instance.scroll.y;
-    },
-    getBoundingClientRect() {
-      return {
-        top: 0,
-        left: 0,
-        width: window.innerWidth,
-        height: window.innerHeight,
-      };
-    },
-  });
-  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-  ScrollTrigger.refresh();
-}
-// loco();
 
 function loader() {
   window.addEventListener("load", () => {
@@ -41,7 +10,7 @@ function loader() {
     });
   });
 }
-// loader();
+ loader();
 
 function boxColor() {
   gsap.from("#page2 .box", {
